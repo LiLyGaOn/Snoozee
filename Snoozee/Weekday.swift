ab01b74be19b38d18b432ce460bdc12ba1d23fe7
GaOn <xnoag@icloud.com>
2023-05-30 18:37:50 +0900

    Feat: Add DataModel & SampleData
    DataModel 구조체 생성 및 SampleData 생성 #10

---
+++ Snoozee/Weekday.swift
@@ -0,0 +1,18 @@
+//
+//  Weekday.swift
+//  Snoozee
+//
+//  Created by xnoag on 2023/05/30.
+//
+
+import Foundation
+
+enum Weekday: String {
+    case mon = "월"
+    case tue = "화"
+    case wed = "수"
+    case thu = "목"
+    case fri = "금"
+    case sat = "토"
+    case sun = "일"
+}
