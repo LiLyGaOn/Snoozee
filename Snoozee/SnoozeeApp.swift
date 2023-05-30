7b9692c32b3b2476886f129fdf3cf9bfa4dae1f0
GaOn <xnoag@icloud.com>
2023-05-30 18:30:53 +0900

    Initial Commit

---
+++ Snoozee/SnoozeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnoozeeApp.swift
+//  Snoozee
+//
+//  Created by xnoag on 2023/05/30.
+//
+
+import SwiftUI
+
+@main
+struct SnoozeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
