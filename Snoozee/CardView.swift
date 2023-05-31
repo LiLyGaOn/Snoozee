7cc236695d23b1418e441852e87b0f906deae5cf
abovocado <dkstjdud02@gmail.com>
2023-05-31 21:18:06 +0900

    Feat: Add CardView
    카드뷰 토글 전까지 View 구현 완료 (텍스트, 색상, 폰트 크기) #2

---
+++ Snoozee/CardView.swift
@@ -0,0 +1,72 @@
+//
+//  CardView.swift
+//  Snoozee
+//
+//  Created by SY AN on 2023/05/31.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    
+
+    var data: DataModel
+    var body: some View {
+        
+        RoundedRectangle(cornerRadius: 24)
+            .aspectRatio(0.73, contentMode: .fit)
+            .foregroundColor(data.isToggleActive ? Color(.systemGreen) : Color(.systemGray4))
+            .opacity(data.isToggleActive ? 0.3 : 1)
+            .overlay(
+                
+                VStack {
+                    HStack {
+                        Text("\(String(format: "%02d", data.hour)):\(String(format: "%02d", data.minute))")
+                            .font(.system(size: UIFont.textStyleSize(.largeTitle) * 1.47058824, weight: .light))
+                            .fixedSize()
+                        Spacer()
+                        
+                    }
+                    
+                    Group {
+                        HStack {
+                            Text("요일: ")
+                                .fixedSize()
+                            Spacer()
+                        }
+                        
+                        HStack {
+                            Text("다시 알림: ")
+                                .fixedSize()
+                            Spacer()
+                        }
+                        
+                        HStack {
+                            Text("사운드: ")
+                                .fixedSize()
+                            Spacer()
+                        }
+                    }
+                    .font(.caption2)
+                    
+                }
+                    .padding()
+                    .foregroundColor(data.isToggleActive ? .white : Color(red:0.459, green: 0.459, blue: 0.459))
+            )
+            
+        
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView(data: DataModel.sampleData[1])
+            .previewLayout(.fixed(width: 169, height: 230))
+    }
+}
+
+public extension UIFont {
+    static func textStyleSize(_ style: UIFont.TextStyle) -> CGFloat {
+        UIFont.preferredFont(forTextStyle: style).pointSize
+    }
+}
