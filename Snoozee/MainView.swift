5701f878ffde3f8a1c4bc4f365c3cfcf01c1e669
GaOn <xnoag@icloud.com>
2023-06-02 16:14:42 +0900

    Feat: Add MainView.Swift
    MainView.Swift 파일 생성 #1

---
+++ Snoozee/MainView.swift
@@ -0,0 +1,23 @@
+//
+//  MainView.swift
+//  Snoozee
+//
+//  Created by xnoag on 2023/06/02.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var data
+    var body: some View {
+        ScrollView {
+            
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
