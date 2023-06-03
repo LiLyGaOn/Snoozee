20dc8968a3dc6d7b3311733be7417e3c1698a418
abovocado <dkstjdud02@gmail.com>
2023-06-03 18:40:40 +0900

    Change: change ReminderView name
    WeekDayView -> ReminderView 오타 수정 #5

--- Snoozee/ReminderView.swift
+++ Snoozee/ReminderView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct WeekDayView: View {
+struct ReminderView: View {
     @State private var selectedRow: Int = 0
     
     let reminderText = ["1분 뒤", "2분 뒤", "3분 뒤", "4분 뒤", "5분 뒤", "10분 뒤", "안 함"]
@@ -37,7 +37,7 @@ struct WeekDayView: View {
 }
     
 
-struct WeekDayView_Previews: PreviewProvider {
+struct ReminderView_Previews: PreviewProvider {
     static var previews: some View {
         WeekDayView()
     }
