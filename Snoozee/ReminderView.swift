b2945f4b3574c4c9be1af22e938bd9b8f2782998
abovocado <dkstjdud02@gmail.com>
2023-06-03 18:34:04 +0900

    Feat: Add ReminderView
    ReminderView 리스트, 체크 기능 구현 #5

---
+++ Snoozee/ReminderView.swift
@@ -0,0 +1,44 @@
+//
+//  ReminderView.swift
+//  Snoozee
+//
+//  Created by SY AN on 2023/06/03.
+//
+
+import SwiftUI
+
+struct WeekDayView: View {
+    @State private var selectedRow: Int = 0
+    
+    let reminderText = ["1분 뒤", "2분 뒤", "3분 뒤", "4분 뒤", "5분 뒤", "10분 뒤", "안 함"]
+    
+    var body: some View {
+        VStack{
+            List {
+                ForEach(reminderText.indices, id: \.self) { index in
+                    Button(action: {
+                        selectedRow = index
+                    }) {
+                        HStack {
+                            Text(reminderText[index])
+                                .foregroundColor(.white)
+                            Spacer()
+                            if selectedRow == index {
+                                Image(systemName: "checkmark")
+                                    .foregroundColor(.yellow)
+                                    .fontWeight(.bold)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+    
+
+struct WeekDayView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeekDayView()
+    }
+}
