527a209bad56105ae1310358e1d69cab1b1362d0
GaOn <xnoag@icloud.com>
2023-06-04 17:34:15 +0900

    Feat: Add Divider
    NavigationTitle과 LazyVGrid 사이에 Divider 추가 #1

--- Snoozee/MainView.swift
+++ Snoozee/MainView.swift
@@ -17,60 +17,64 @@ struct MainView: View {
     var body: some View {
         NavigationView{
             ScrollView {
-                LazyVGrid(columns: gridColumns) {
-                    ForEach(dataModels.indices) { index in
-                        if index == 0 {
-                            Button(action: {
-                                isShowingSheet = true
-                                parameterData = DataModel.sampleData[0]
-                                parameterIsEditing = false
-                            }) {
-                                RoundedRectangle(cornerRadius: 24)
-                                    .aspectRatio(0.73, contentMode: .fit)
-                                    .foregroundColor(Color(.systemGray4))
-                                    .overlay (
-                                        Image(systemName: "plus")
-                                            .font(.largeTitle)
-                                            .foregroundColor(Color(.systemOrange))
-                                    )}} else {
-                                        
-                                        Button(action: {
-                                            isShowingSheet = true
-                                            parameterData = dataModels[index]
-                                            parameterIsEditing = true
+                VStack {
+                    Divider()
+                        .padding(.vertical)
+                    LazyVGrid(columns: gridColumns) {
+                        ForEach(dataModels.indices) { index in
+                            if index == 0 {
+                                Button(action: {
+                                    isShowingSheet = true
+                                    parameterData = DataModel.sampleData[0]
+                                    parameterIsEditing = false
+                                }) {
+                                    RoundedRectangle(cornerRadius: 24)
+                                        .aspectRatio(0.73, contentMode: .fit)
+                                        .foregroundColor(Color(.systemGray4))
+                                        .overlay (
+                                            Image(systemName: "plus")
+                                                .font(.largeTitle)
+                                                .foregroundColor(Color(.systemOrange))
+                                        )}} else {
                                             
-                                        }){
-                                            CardView(dataModel: dataModels[index])
+                                            Button(action: {
+                                                isShowingSheet = true
+                                                parameterData = dataModels[index]
+                                                parameterIsEditing = true
+                                                
+                                            }){
+                                                CardView(dataModel: dataModels[index])
+                                            }
                                         }
-                                    }
+                        }
                     }
                 }
-            }
-            .padding(.horizontal)
-            .navigationTitle("스누즈")
-            .sheet(isPresented: $isShowingSheet) {
-                NavigationView{
-                    SettingView(dataModel: parameterData, isEditing: parameterIsEditing) 
-                        .toolbar {
-                            ToolbarItem(placement: .cancellationAction){
-                                Button("취소"){
-                                    isShowingSheet = false
+                .padding(.horizontal)
+                .navigationTitle("스누즈")
+                .sheet(isPresented: $isShowingSheet) {
+                    NavigationView{
+                        SettingView(dataModel: parameterData, isEditing: parameterIsEditing)
+                            .toolbar {
+                                ToolbarItem(placement: .cancellationAction){
+                                    Button("취소"){
+                                        isShowingSheet = false
+                                    }
+                                    .foregroundColor(Color(.systemOrange))
                                 }
-                                .foregroundColor(Color(.systemOrange))
-                            }
-                            ToolbarItem(placement: .principal) {
-                                Text(parameterIsEditing ? "알람 편집" : "알람 추가")
-                                    .fontWeight(.bold)
-                            }
-                            
-                            
-                            ToolbarItem(placement: .confirmationAction){
-                                Button("저장"){
-                                    isShowingSheet = false
+                                ToolbarItem(placement: .principal) {
+                                    Text(parameterIsEditing ? "알람 편집" : "알람 추가")
+                                        .fontWeight(.bold)
                                 }
-                                .foregroundColor(Color(.systemOrange))
+                                
+                                
+                                ToolbarItem(placement: .confirmationAction){
+                                    Button("저장"){
+                                        isShowingSheet = false
+                                    }
+                                    .foregroundColor(Color(.systemOrange))
+                                }
                             }
-                        }
+                    }
                 }
             }
         }
