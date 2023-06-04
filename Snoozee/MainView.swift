64f45ac05fa954da33beba0aa3957f15e45b2f74
abovocado <dkstjdud02@gmail.com>
2023-06-04 16:20:20 +0900

    Feat: Add Setting View link
    MainView - SettingView 연결 및 ToolBar 추가 완료 #1

--- Snoozee/MainView.swift
+++ Snoozee/MainView.swift
@@ -8,19 +8,71 @@
 import SwiftUI
 
 struct MainView: View {
-    var dataModels: [DataModel]
+    @State var dataModels: [DataModel]
     @State var gridColumns = Array(repeating: GridItem(.flexible()), count: 2)
+    @State var isShowingSheet: Bool = false
+    @State var parameterData: DataModel = DataModel.sampleData[0]
+    @State var parameterIsEditing: Bool = false
+    
     var body: some View {
         NavigationView{
             ScrollView {
                 LazyVGrid(columns: gridColumns) {
-                    ForEach(dataModels) { dataModel in
-                        CardView(dataModel: dataModel)
+                    ForEach(dataModels.indices) { index in
+                        if index == 0 {
+                            Button(action: {
+                                isShowingSheet = true
+                                parameterData = DataModel.sampleData[0]
+                                parameterIsEditing = false
+                            }) {
+                                RoundedRectangle(cornerRadius: 24)
+                                    .aspectRatio(0.73, contentMode: .fit)
+                                    .foregroundColor(Color(.systemGray4))
+                                    .overlay (
+                                        Image(systemName: "plus")
+                                            .font(.system(size: 24))
+                                            .foregroundColor(Color(.systemOrange))
+                                    )}} else {
+                                        
+                                        Button(action: {
+                                            isShowingSheet = true
+                                            parameterData = dataModels[index]
+                                            parameterIsEditing = true
+                                            
+                                        }){
+                                            CardView(dataModel: dataModels[index])
+                                        }
+                                    }
                     }
                 }
             }
             .padding(.horizontal)
             .navigationTitle("스누즈")
+            .sheet(isPresented: $isShowingSheet) {
+                NavigationView{
+                    SettingView(dataModel: parameterData, isEditing: parameterIsEditing)
+                        .toolbar {
+                            ToolbarItem(placement: .cancellationAction){
+                                Button("취소"){
+                                    isShowingSheet = false
+                                }
+                                .foregroundColor(Color(.systemOrange))
+                            }
+                            ToolbarItem(placement: .principal) {
+                                Text(parameterIsEditing ? "알람 편집" : "알람 추가")
+                                    .fontWeight(.bold)
+                            }
+                            
+                            
+                            ToolbarItem(placement: .confirmationAction){
+                                Button("저장"){
+                                    isShowingSheet = false
+                                }
+                                .foregroundColor(Color(.systemOrange))
+                            }
+                        }
+                }
+            }
         }
     }
 }
